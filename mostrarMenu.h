a28eddc27c50c1f7d2c86368a9a00cfe1facaa68
Fernando270617 <107822824+Fernando270617@users.noreply.github.com>
2025-05-17 12:07:42 -0500

    mostrarMenu.h

---
+++ mostrarMenu.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Función para mostrar el menú al usuario
+
+void mostrarMenu() {
+    printf("\nMenú de opciones:\n");
+    printf("1. Agregar producto\n");
+    printf("2. Editar producto\n");
+    printf("3. Buscar producto registrado\n");
+    printf("4. Eliminar producto\n");
+    printf("5. Calcular tiempo y recursos\n");
+    printf("6. Verificar cumplimiento de demanda\n");
+    printf("7. Salir\n");
+    printf("Seleccione una opción: ");
+}
